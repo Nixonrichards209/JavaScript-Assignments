c6765f63538b594d3773cd82ec66fa48b8efa360
Nixonrichards209 <nic.ric209@gmail.com>
2025-10-23 15:29:47 +0530

    Initial commit of JS assignments

---
+++ primitive_datatypes.js
@@ -0,0 +1,25 @@
+/**
+    Create the following variables using let (not using var) and check their typeOf 
+    a) firstName 
+    b) companyName 
+    c) mobileNumber 
+    d) isAutomation 
+    e) hasPlaywright (do not assign) 
+ Print and confirm the values and data types 
+
+
+ */
+
+
+ let firstName = "Nixon"
+ let companyName = "Mastek"
+ let mobileNumber = "9962026676"
+ let isAutomation = true
+ let hasPlaywright = undefined
+
+console.log (`The datatype of the variable ${firstName} is` + ` `+ typeof(firstName))
+console.log (`The datatype of the variable ${companyName} is` + ` `+ typeof(companyName))
+console.log (`The datatype of the variable ${mobileNumber} is` + ` `+ typeof(mobileNumber))
+console.log (`The datatype of the variable ${isAutomation} is` + ` `+ typeof(isAutomation))
+console.log (`The datatype of the variable ${hasPlaywright} is` + ` `+ typeof(hasPlaywright))
+
