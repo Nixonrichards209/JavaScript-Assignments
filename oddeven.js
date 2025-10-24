19d6c1a6230bb596a4c85f778c38b1b8208bbac1
Nixonrichards209 <nic.ric209@gmail.com>
2025-10-24 16:37:53 +0530

    Add oddOrEven function to check even/odd numbers

---
+++ oddeven.js
@@ -0,0 +1,17 @@
+var oddOrEven = function () {
+
+let x = 12
+
+if (x%2==0) {
+
+console.log(`The entered number is Even`)
+
+} else {
+
+console.log(`The entered number is Odd`)
+
+}
+
+}
+
+oddOrEven()
