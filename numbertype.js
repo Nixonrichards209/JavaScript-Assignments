8f16422d8b6bf8fc572dffda2acb77d5dd7d041a
Nixonrichards209 <nic.ric209@gmail.com>
2025-10-24 21:30:22 +0530

    Number Type finding Exercise

---
+++ numbertype.js
@@ -0,0 +1,21 @@
+function numberType () {
+
+let x = 0
+
+if (x>0) {
+
+console.log(`The given number is Positive`)
+
+} else if (x<0) {
+
+console.log(`The given number is Negative`)
+    
+} else if (x==0) {
+
+console.log(`The given number is Zero`)
+
+}
+
+}
+
+numberType()
